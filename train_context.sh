06db9a42b3dfa17d8759caad5851e697f430ee4c
Nicolas Garneau <nicolas.garneau.1@ulaval.ca>
2020-06-04 09:36:56 -0400

    use setting and epoch env var

--- train_context.sh
+++ train_context.sh
@@ -1,9 +1,9 @@
 seed=$SEED
 encoder=$ENCODER
-epochs=5
+epochs=$EPOCHS
 bs=16
 loss=hardtriplet
-setting=unique
+setting=$SETTING
 eval_steps=$EVALSTEPS
 context=$CONTEXT
 name=$NAME
@@ -11,5 +11,5 @@ name=$NAME
 # for lang in en da de es fi fr it nl pl pt sv; do
 # for lang in en; do
 outdir=./output/${name}_context_${context}_${encoder}_whole_${setting}_${loss}_${epochs}_${bs}_${seed}
-python -m sentence_transformers.train --loss $loss --data_path ./data/analogy_contexts_splits --train_data all_languages_unique.csv.train --dev_data analogy_${setting}_en_contexts.csv.valid --bs $bs --encoder $encoder --epochs $epochs --evaluation_steps $eval_steps --out $outdir --seed $seed --context $context
+python -m sentence_transformers.train --loss $loss --data_path ./data/analogy_contexts_splits --train_data all_languages_${setting}.csv.train --dev_data analogy_${setting}_en_contexts.csv.valid --bs $bs --encoder $encoder --epochs $epochs --evaluation_steps $eval_steps --out $outdir --seed $seed --context $context
 # done
